c0eed739be400e020bb1ce915f5a4f09ec214e19
fariztaufiks <fariztaufiks3108@gmail.com>
2025-05-22 11:11:37 +0700

    algo5

--- Algo5/ptb.cpp
+++ Algo5/ptb.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -10,7 +9,7 @@ struct PtbNode {
 
     PtbNode(int val){
             data = val;
-            left = left = nullptr;
+            left = right = nullptr;
     }
 };
 
@@ -19,4 +18,12 @@ int main(){
       root->left = new PtbNode(10);
       root->right = new PtbNode(20);
       root->left->left = new PtbNode(9);
+      root->left->right = new PtbNode(12);
+
+      cout << "Root: " << root->data << endl;
+      cout << "Anak kiri dari root: " << root->left->data << endl;
+      cout << "Anak kanan dari root: " << root->right->data << endl;
+      cout << "Anak kiri dari PTB Node 2: " << root->left->left->data << endl;
+      cout << "Anak kanan dari PTB Node 2: " << root->left->right->data << endl;
+
 }
